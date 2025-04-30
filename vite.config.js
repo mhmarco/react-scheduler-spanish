7b7ce270631e8cdf896deaa411582d6a3e20688c
Marco <mhmarco.dev@gmail.com>
2025-04-29 22:28:14 -0600

    final build version

--- vite.config.js
+++ vite.config.js
@@ -50,8 +50,8 @@ export default defineConfig({
         }
       }
     },
-    sourcemap: true, //remove these lines to debug
-    minify: false
+    //sourcemap: true, //remove these lines to debug
+    //minify: false
   },
   server: {
     host: "0.0.0.0"
